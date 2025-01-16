6e60021a1f98234a6e35553497d1d058ae80fad5
LordIdra <percy.baker.0@gmail.com>
2025-01-16 21:00:52 +0000

    Add ChunkPosition

---
+++ src/main/kotlin/io/github/pylonmc/pylon/core/block/ChunkPosition.kt
@@ -0,0 +1,25 @@
+package io.github.pylonmc.pylon.core.block
+
+import org.bukkit.Chunk
+import org.bukkit.Location
+import org.bukkit.block.Block
+
+class ChunkPosition(x: Int, z: Int) {
+    private val asLong = x.toLong().shl(32).or(z.toLong().and(0xFFFFFFFFL))
+    val x = asLong.shr(32).toInt()
+    val y = asLong.toInt()
+
+    constructor(chunk: Chunk) : this(chunk.x, chunk.z)
+
+    constructor(location: Location) : this(location.chunk)
+
+    constructor(block: Block) : this(block.chunk)
+
+    override fun hashCode(): Int {
+        return asLong.hashCode()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return (other is ChunkPosition) && other.asLong == asLong
+    }
+}
