8247fcb856a04c5494da0bcd0f13417a63df8b72
Seggan <seggan21@gmail.com>
2025-01-15 21:14:28 -0500

    Initial commit

---
+++ src/main/kotlin/io/github/pylonmc/pylon/core/PylonCore.kt
@@ -0,0 +1,24 @@
+package io.github.pylonmc.pylon.core
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class PylonCore : JavaPlugin() {
+
+    override fun onEnable() {
+        instance = this
+        logger.info("Hello, World!")
+    }
+
+    override fun onDisable() {
+        instance = null
+    }
+
+    companion object {
+        var instance: PylonCore? = null
+            private set
+    }
+}
+
+// for internal use so we don't have to !! all the time
+internal val pluginInstance: PylonCore
+    get() = PylonCore.instance ?: error("PyloCore instance is not initialized")
