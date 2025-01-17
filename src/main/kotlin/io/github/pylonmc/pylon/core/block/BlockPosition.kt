48e31c7884c78ddb21bc6de98b8e379b6fa1f92b
LordIdra <percy.baker.0@gmail.com>
2025-01-17 02:04:23 +0000

    formatting

--- src/main/kotlin/io/github/pylonmc/pylon/core/block/BlockPosition.kt
+++ src/main/kotlin/io/github/pylonmc/pylon/core/block/BlockPosition.kt
@@ -10,16 +10,14 @@ class BlockPosition(world: World, x: Int, y: Int, z: Int) {
     private var asLong: Long = ((x and 0x3FFFFFF).toLong() shl 38)
         .or((z and 0x3FFFFFF).toLong() shl 12)
         .or((y and 0xFFF).toLong())
+    val x: Int = (asLong shr 38).toInt()
+    val y: Int = ((asLong shl 52) shr 52).toInt()
+    val z: Int = ((asLong shl 26) shr 38).toInt()
+    val chunk = ChunkPosition(world, x shr 4, z shr 4)
 
     constructor(location: Location) : this(location.world, location.blockX, location.blockY, location.blockZ)
 
     constructor(block: Block) : this(block.world, block.x, block.y, block.z)
-
-    val x: Int = (asLong shr 38).toInt()
-    val y: Int = ((asLong shl 52) shr 52).toInt()
-    val z: Int = ((asLong shl 26) shr 38).toInt()
-
-    val chunk = ChunkPosition(world, x shr 4, z shr 4)
 
     override fun hashCode(): Int {
         val prime = 31
