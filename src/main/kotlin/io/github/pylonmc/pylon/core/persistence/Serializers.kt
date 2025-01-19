ec89060abd9229ccbb3ded36ad31b22ef5544738
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-01-19 18:15:51 -0500

    Added serializers for Vector datatype

--- src/main/kotlin/io/github/pylonmc/pylon/core/persistence/Serializers.kt
+++ src/main/kotlin/io/github/pylonmc/pylon/core/persistence/Serializers.kt
@@ -1,8 +1,10 @@
 package io.github.pylonmc.pylon.core.persistence
 
+import org.bukkit.Location
 import org.bukkit.NamespacedKey
 import org.bukkit.persistence.PersistentDataAdapterContext
 import org.bukkit.persistence.PersistentDataType
+import org.bukkit.util.Vector
 import java.nio.ByteBuffer
 import java.util.*
 
@@ -55,6 +57,30 @@ class UUIDPersistentDataType : PersistentDataType<ByteArray, UUID> {
         val buffer = ByteBuffer.allocate(2 * Long.SIZE_BYTES)
         buffer.putLong(complex.mostSignificantBits)
         buffer.putLong(complex.leastSignificantBits)
+        return buffer.array()
+    }
+}
+
+class VectorPersistentDataType : PersistentDataType<ByteArray, Vector> {
+    override fun getPrimitiveType(): Class<ByteArray>
+        = ByteArray::class.java
+
+    override fun getComplexType(): Class<Vector>
+        = Vector::class.java
+
+    override fun fromPrimitive(primitive: ByteArray, context: PersistentDataAdapterContext): Vector {
+        val buffer = ByteBuffer.wrap(primitive)
+        val x = buffer.getDouble()
+        val y = buffer.getDouble()
+        val z = buffer.getDouble()
+        return Vector(x, y, z)
+    }
+
+    override fun toPrimitive(complex: Vector, context: PersistentDataAdapterContext): ByteArray {
+        val buffer = ByteBuffer.allocate(3 * Double.SIZE_BYTES)
+        buffer.putDouble(complex.x)
+        buffer.putDouble(complex.y)
+        buffer.putDouble(complex.z)
         return buffer.array()
     }
 }
