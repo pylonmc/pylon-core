92e5d88989858deba72139082d0e66067dc5b5ca
Seggan <seggan21@gmail.com>
2025-01-20 13:32:41 -0500

    Shutdown on done

--- pylon-test/src/main/java/io/github/pylonmc/pylon/test/TestAddon.java
+++ pylon-test/src/main/java/io/github/pylonmc/pylon/test/TestAddon.java
@@ -1,6 +1,7 @@
 package io.github.pylonmc.pylon.test;
 
 import io.github.pylonmc.pylon.core.addon.PylonAddon;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
@@ -9,6 +10,8 @@ public class TestAddon extends JavaPlugin implements PylonAddon {
     @Override
     public void onEnable() {
         getLogger().info("Test addon enabled!");
+
+        Bukkit.getScheduler().runTask(this, Bukkit::shutdown);
     }
 
     @Override
