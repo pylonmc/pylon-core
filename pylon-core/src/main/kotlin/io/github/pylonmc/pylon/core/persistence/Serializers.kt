fa97306c939a08b2b495df46a85f5e036d02ea5b
MetaMechanists <35176119+LordIdra@users.noreply.github.com>
2025-01-22 15:28:39 +0000

    NamespaceKeyPDT primitive -> string, and UUID primitive -> LongArray

--- pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/persistence/Serializers.kt
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/persistence/Serializers.kt
@@ -3,59 +3,67 @@ package io.github.pylonmc.pylon.core.persistence
 import org.bukkit.NamespacedKey
 import org.bukkit.persistence.PersistentDataAdapterContext
 import org.bukkit.persistence.PersistentDataType
-import java.nio.ByteBuffer
 import java.util.*
 
 object Serializers {
+    @JvmField
     val BYTE = PersistentDataType.BYTE!!
+    @JvmField
     val SHORT = PersistentDataType.SHORT!!
+    @JvmField
     val INTEGER = PersistentDataType.INTEGER!!
+    @JvmField
     val LONG = PersistentDataType.LONG!!
+    @JvmField
     val FLOAT = PersistentDataType.FLOAT!!
+    @JvmField
     val DOUBLE = PersistentDataType.DOUBLE!!
+    @JvmField
     val BOOLEAN = PersistentDataType.BOOLEAN!!
+    @JvmField
     val STRING = PersistentDataType.STRING!!
+    @JvmField
     val BYTE_ARRAY = PersistentDataType.BYTE_ARRAY!!
+    @JvmField
     val INTEGER_ARRAY = PersistentDataType.INTEGER_ARRAY!!
+    @JvmField
     val LONG_ARRAY = PersistentDataType.LONG_ARRAY!!
+    @JvmField
     val TAG_CONTAINER = PersistentDataType.TAG_CONTAINER!!
+    @JvmField
     val LIST = PersistentDataType.LIST!!
+    @JvmField
     val NAMESPACED_KEY = NamespacedKeyPersistentDataType()
+    @JvmField
     val UUID = UUIDPersistentDataType()
 }
 
-class NamespacedKeyPersistentDataType : PersistentDataType<ByteArray, NamespacedKey> {
-    override fun getPrimitiveType(): Class<ByteArray>
-        = ByteArray::class.java
+class NamespacedKeyPersistentDataType : PersistentDataType<String, NamespacedKey> {
+    override fun getPrimitiveType(): Class<String>
+        = String::class.java
 
     override fun getComplexType(): Class<NamespacedKey>
         = NamespacedKey::class.java
 
-    override fun fromPrimitive(primitive: ByteArray, context: PersistentDataAdapterContext): NamespacedKey
-        = NamespacedKey.fromString(primitive.toString(Charsets.UTF_8))!!
+    override fun fromPrimitive(primitive: String, context: PersistentDataAdapterContext): NamespacedKey
+        = NamespacedKey.fromString(primitive)!!
 
-    override fun toPrimitive(complex: NamespacedKey, context: PersistentDataAdapterContext): ByteArray
-        = complex.toString().toByteArray(Charsets.UTF_8)
+    override fun toPrimitive(complex: NamespacedKey, context: PersistentDataAdapterContext): String
+        = complex.toString()
 }
 
-class UUIDPersistentDataType : PersistentDataType<ByteArray, UUID> {
-    override fun getPrimitiveType(): Class<ByteArray>
-            = ByteArray::class.java
+class UUIDPersistentDataType : PersistentDataType<LongArray, UUID> {
+    override fun getPrimitiveType(): Class<LongArray>
+            = LongArray::class.java
 
     override fun getComplexType(): Class<UUID>
             = UUID::class.java
 
-    override fun fromPrimitive(primitive: ByteArray, context: PersistentDataAdapterContext): UUID {
-        val buffer = ByteBuffer.wrap(primitive)
-        val mostSignificantBits = buffer.getLong()
-        val leastSignificantBits = buffer.getLong()
-        return UUID(mostSignificantBits , leastSignificantBits)
+    override fun fromPrimitive(primitive: LongArray, context: PersistentDataAdapterContext): UUID {
+        return UUID(primitive[0], primitive[1])
     }
 
-    override fun toPrimitive(complex: UUID, context: PersistentDataAdapterContext): ByteArray {
-        val buffer = ByteBuffer.allocate(2 * Long.SIZE_BYTES)
-        buffer.putLong(complex.mostSignificantBits)
-        buffer.putLong(complex.leastSignificantBits)
-        return buffer.array()
+    override fun toPrimitive(complex: UUID, context: PersistentDataAdapterContext): LongArray {
+        return longArrayOf(complex.mostSignificantBits, complex.leastSignificantBits)
     }
 }
