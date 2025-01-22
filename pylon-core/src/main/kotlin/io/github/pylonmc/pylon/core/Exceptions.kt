6d8a1d0f347753d11a28ab3d4657970e1c12af9e
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-01-22 18:13:53 -0500

    au revoir to exceptions.kt

--- pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/Exceptions.kt
+++
@@ -1,8 +0,0 @@
-package io.github.pylonmc.pylon.core
-
-class SchemaNotFoundException(id: String) : Exception("Schema '$id' not found")
-class NotRegisteredException(id: String) : Exception("'$id' is not registered")
-class AlreadyRegisteredException(id: String) : Exception("'$id' is already registered")
-class MissingPlaceConstructorException(id: String) : Exception("'$id' is missing a place constructor")
-class MissingLoadConstructorException(id: String) : Exception("'$id' is missing a load constructor")
-class InvalidPrimitiveTypeException(type: Class<out Any?>) : Exception("Invalid primitive type '$type'")
