12e6c10135b406fa99da7c922cf1dc917bb30d1e
MetaMechanists <35176119+LordIdra@users.noreply.github.com>
2025-01-22 22:02:45 +0000

    Add set and map datatype

--- pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/persistence/Serializers.kt
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/persistence/Serializers.kt
@@ -1,7 +1,9 @@
 package io.github.pylonmc.pylon.core.persistence
 
+import io.github.pylonmc.pylon.core.pluginInstance
 import org.bukkit.NamespacedKey
 import org.bukkit.persistence.PersistentDataAdapterContext
+import org.bukkit.persistence.PersistentDataContainer
 import org.bukkit.persistence.PersistentDataType
 import java.util.*
 
@@ -31,11 +33,15 @@ object Serializers {
     @JvmField
     val TAG_CONTAINER = PersistentDataType.TAG_CONTAINER!!
     @JvmField
-    val LIST = PersistentDataType.LIST!!
-    @JvmField
     val NAMESPACED_KEY = NamespacedKeyPersistentDataType()
     @JvmField
     val UUID = UUIDPersistentDataType()
+    @JvmField
+    val LIST = PersistentDataType.LIST!!
+    @JvmField
+    val SET = SetPersistentDataTypeProvider()
+    @JvmField
+    val MAP = MapPersistentDataTypeProvider()
 }
 
 class NamespacedKeyPersistentDataType : PersistentDataType<String, NamespacedKey> {
@@ -65,5 +71,96 @@ class UUIDPersistentDataType : PersistentDataType<LongArray, UUID> {
 
     override fun toPrimitive(complex: UUID, context: PersistentDataAdapterContext): LongArray {
         return longArrayOf(complex.mostSignificantBits, complex.leastSignificantBits)
+    }
+}
+
+interface SetPersistentDataType<P, C> : PersistentDataType<PersistentDataContainer, Set<C>> {
+    fun elementType(): PersistentDataType<P, C>
+}
+
+// 'Hold on, why the hell are we returning an entire PDC instead of a list of primitives???'
+// Well for some reason, lists are only counted as primitives if accompanied by a ListPersistentDataType (wtf)
+class SetPersistentDataTypeProvider {
+    fun <P: Any, C: Any> setTypeFrom(elementType: PersistentDataType<P, C>): SetPersistentDataType<P, C>
+            = SetPersistentDataTypeImpl(elementType)
+
+    private class SetPersistentDataTypeImpl<P: Any, C: Any>(
+        private val elementType: PersistentDataType<P, C>
+    ) : SetPersistentDataType<P, C> {
+
+        private val setValues = NamespacedKey(pluginInstance, "values")
+
+        override fun elementType(): PersistentDataType<P, C>
+            = elementType
+
+        override fun getPrimitiveType(): Class<PersistentDataContainer>
+            = PersistentDataContainer::class.java
+
+        @Suppress("UNCHECKED_CAST") // Yes, this is cursed, no, there's no way around it afaik
+        override fun getComplexType(): Class<Set<C>>
+            = Set::class.java as Class<Set<C>>
+
+        override fun toPrimitive(complex: Set<C>, context: PersistentDataAdapterContext): PersistentDataContainer {
+            val pdc = context.newPersistentDataContainer()
+            pdc.set(setValues, PersistentDataType.LIST.listTypeFrom(elementType), ArrayList(complex))
+            return pdc
+        }
+
+        override fun fromPrimitive(primitive: PersistentDataContainer, context: PersistentDataAdapterContext): Set<C>
+            = primitive.get(setValues, PersistentDataType.LIST.listTypeFrom(elementType))!!.toSet()
+    }
+}
+
+interface MapPersistentDataType<KP, KC, VP, VC> : PersistentDataType<PersistentDataContainer, Map<KC, VC>> {
+    fun keyType(): PersistentDataType<KP, KC>
+    fun valueType(): PersistentDataType<VP, VC>
+}
+
+class MapPersistentDataTypeProvider {
+    fun <KP: Any, KC: Any, VP: Any, VC: Any> mapTypeFrom(
+        keyType: PersistentDataType<KP, KC>,
+        valueType: PersistentDataType<VP, VC>
+    ): MapPersistentDataType<KP, KC, VP, VC>
+        = MapPersistentDataTypeImpl(keyType, valueType)
+
+    private class MapPersistentDataTypeImpl<KP: Any, KC: Any, VP: Any, VC: Any>(
+        private val keyType: PersistentDataType<KP, KC>,
+        private val valueType: PersistentDataType<VP, VC>
+    ) : MapPersistentDataType<KP, KC, VP, VC> {
+
+        private val mapKeys = NamespacedKey(pluginInstance, "keys")
+        private val mapValues = NamespacedKey(pluginInstance, "values")
+
+        override fun keyType(): PersistentDataType<KP, KC>
+            = keyType
+
+        override fun valueType(): PersistentDataType<VP, VC>
+            = valueType
+
+        override fun getPrimitiveType(): Class<PersistentDataContainer>
+            = PersistentDataContainer::class.java
+
+        @Suppress("UNCHECKED_CAST")
+        override fun getComplexType(): Class<Map<KC, VC>>
+            = Map::class.java as Class<Map<KC, VC>>
+
+        override fun toPrimitive(
+            complex: Map<KC, VC>,
+            context: PersistentDataAdapterContext
+        ): PersistentDataContainer {
+            val primitive = context.newPersistentDataContainer()
+            primitive.set(mapKeys, Serializers.LIST.listTypeFrom(keyType), complex.keys.toList())
+            primitive.set(mapValues, Serializers.LIST.listTypeFrom(valueType), complex.values.toList())
+            return primitive
+        }
+
+        override fun fromPrimitive(
+            primitive: PersistentDataContainer,
+            context: PersistentDataAdapterContext
+        ): Map<KC, VC> {
+            val keys = primitive.get(mapKeys, Serializers.LIST.listTypeFrom(keyType))!!
+            val values = primitive.get(mapValues, Serializers.LIST.listTypeFrom(valueType))!!
+            return keys.zip(values).toMap()
+        }
     }
 }
