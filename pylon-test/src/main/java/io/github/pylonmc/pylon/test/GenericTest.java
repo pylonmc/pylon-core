1b422884ecea605c9ebde2e6b8ad84ea333adaac
MetaMechanists <35176119+LordIdra@users.noreply.github.com>
2025-01-24 19:14:14 +0000

    Fix test name in key

--- pylon-test/src/main/java/io/github/pylonmc/pylon/test/GenericTest.java
+++ pylon-test/src/main/java/io/github/pylonmc/pylon/test/GenericTest.java
@@ -4,7 +4,7 @@ import org.bukkit.NamespacedKey;
 
 public interface GenericTest {
     default NamespacedKey getKey() {
-        return new NamespacedKey(TestAddon.instance(), this.getClass().getName());
+        return new NamespacedKey(TestAddon.instance(), this.getClass().getSimpleName());
     }
     void run();
     void cleanup();
