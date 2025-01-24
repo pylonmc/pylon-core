9cd75b23d9340b9b05a3999a4a0429e172630132
MetaMechanists <35176119+LordIdra@users.noreply.github.com>
2025-01-24 19:01:01 +0000

    Fix mistake during rebase

--- pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/test/GameTestConfig.kt
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/test/GameTestConfig.kt
@@ -9,8 +9,8 @@ import java.util.concurrent.CompletableFuture
 import java.util.function.Consumer
 import kotlin.properties.Delegates
 
-data class GameTestConfig(
-    val key: NamespacedKey,
+class GameTestConfig(
+    private val key: NamespacedKey,
     val size: Int,
     val setUp: (GameTest) -> Unit,
     val delayTicks: Int,
