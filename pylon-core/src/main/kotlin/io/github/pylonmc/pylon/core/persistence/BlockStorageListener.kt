2b2c96ca074a41c2b222862cd9f03ef19389674e
LordIdra <percy.baker.0@gmail.com>
2025-01-28 01:06:39 +0000

    Add more blockstorage listeners

--- pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/persistence/BlockStorageListener.kt
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/persistence/BlockStorageListener.kt
@@ -3,8 +3,18 @@ package io.github.pylonmc.pylon.core.persistence
 import io.github.pylonmc.pylon.core.block.position
 import io.github.pylonmc.pylon.core.pluginInstance
 import org.bukkit.Bukkit
+import org.bukkit.ExplosionResult
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockBreakEvent
+import org.bukkit.event.block.BlockBurnEvent
+import org.bukkit.event.block.BlockExplodeEvent
+import org.bukkit.event.block.BlockFadeEvent
+import org.bukkit.event.block.BlockFormEvent
+import org.bukkit.event.block.BlockFromToEvent
+import org.bukkit.event.block.BlockPistonExtendEvent
+import org.bukkit.event.block.BlockPistonRetractEvent
+import org.bukkit.event.entity.EntityExplodeEvent
 import org.bukkit.event.world.ChunkLoadEvent
 import org.bukkit.event.world.ChunkUnloadEvent
 
@@ -14,12 +24,76 @@ internal object BlockStorageListener : Listener {
     }
 
     @EventHandler
-    fun onChunkLoad(event: ChunkLoadEvent) {
+    fun chunkLoad(event: ChunkLoadEvent) {
         BlockStorage.load(event.chunk.position)
     }
 
     @EventHandler
-    fun onChunkUnload(event: ChunkUnloadEvent) {
+    fun chunkSave(event: ChunkUnloadEvent) {
         BlockStorage.save(event.chunk.position)
+    }
+
+    @EventHandler
+    fun blockRemove(event: BlockBreakEvent) {
+        BlockStorage.remove(event.block.position)
+    }
+
+    @EventHandler
+    fun blockBurn(event: BlockBurnEvent) {
+        BlockStorage.remove(event.block.position)
+    }
+
+    @EventHandler
+    fun blockRemove(event: BlockExplodeEvent) {
+        BlockStorage.remove(event.block.position)
+        for (block in event.blockList()) {
+            BlockStorage.remove(block.position)
+        }
+    }
+
+    @EventHandler
+    fun blockRemove(event: EntityExplodeEvent) {
+        for (block in event.blockList()) {
+            BlockStorage.remove(block.position)
+        }
+    }
+
+    @EventHandler
+    fun blockRemove(event: BlockFadeEvent) {
+        BlockStorage.remove(event.block.position)
+    }
+
+    @EventHandler
+    fun disallowForming(event: BlockFormEvent) {
+        if (BlockStorage.exists(event.block.position)) {
+            event.isCancelled = true
+        }
+    }
+
+    @EventHandler
+    fun disallowFromTo(event: BlockFromToEvent) {
+        if (BlockStorage.exists(event.block.position)) {
+            event.isCancelled = true
+        }
+    }
+
+    @EventHandler
+    fun disallowMovementByPistons(event: BlockPistonExtendEvent) {
+        for (block in event.blocks) {
+            if (BlockStorage.exists(block.position)) {
+                event.isCancelled = true
+                return
+            }
+        }
+    }
+
+    @EventHandler
+    fun disallowMovementByPistons(event: BlockPistonRetractEvent) {
+        for (block in event.blocks) {
+            if (BlockStorage.exists(block.position)) {
+                event.isCancelled = true
+                return
+            }
+        }
     }
 }
