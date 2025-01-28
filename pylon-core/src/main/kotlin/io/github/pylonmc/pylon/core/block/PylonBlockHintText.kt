b8efa1fcf6ef1a310fa40e2914af7f57c928e66d
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-01-28 07:43:02 -0500

    Change PylonBlockHintText class to hold bossbar in class instead of just string

--- pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/block/PylonBlockHintText.kt
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/block/PylonBlockHintText.kt
@@ -7,17 +7,30 @@ import org.bukkit.boss.BossBar
 import org.bukkit.entity.Player
 
 
-open class PylonBlockHintText(open var hintText: String, open var hintColor: BarColor, open var hintStyle: BarStyle) {
-    private val activeHints: MutableMap<Player, BossBar> = HashMap()
+open class PylonBlockHintText(hintText: String, hintColor: BarColor, hintStyle: BarStyle) {
+    private var bossBar = Bukkit.createBossBar(hintText, hintColor, hintStyle)
     fun activateFor(player: Player){
-        val bossBar = Bukkit.createBossBar(hintText, hintColor, hintStyle)
         bossBar.addPlayer(player)
-        activeHints[player] = bossBar
     }
     fun deactivateFor(player: Player){
-        if(activeHints.containsKey(player)){
-            activeHints[player]!!.removeAll()
-            activeHints.remove(player)
-        }
+        bossBar.removePlayer(player)
+    }
+    fun changeHintText(newHintText: String){
+        bossBar.setTitle(newHintText)
+    }
+    fun changeHintColor(newHintColor: BarColor){
+        bossBar.color = newHintColor
+    }
+    fun changeHintStyle(newHintStyle: BarStyle){
+        bossBar.style = newHintStyle
+    }
+    fun getHintText(): String {
+        return bossBar.title
+    }
+    fun getHintColor(): BarColor {
+        return bossBar.color
+    }
+    fun getHintStyle(): BarStyle {
+        return bossBar.style
     }
 }
