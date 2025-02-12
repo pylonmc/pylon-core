14570c384b143d5e1ba18ac42cea18fa96cd0efc
Seggan <seggan21@gmail.com>
2025-02-12 13:39:33 -0500

    Pass whole event to mob drop

--- pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/recipe/MobDrop.kt
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/recipe/MobDrop.kt
@@ -1,10 +1,10 @@
 package io.github.pylonmc.pylon.core.recipe
 
 import org.bukkit.Keyed
-import org.bukkit.entity.LivingEntity
+import org.bukkit.event.entity.EntityDeathEvent
 import org.bukkit.inventory.ItemStack
 
 interface MobDrop : Keyed {
 
-    fun getResult(entity: LivingEntity, playerKill: Boolean): ItemStack?
+    fun getResult(event: EntityDeathEvent): ItemStack?
 }
