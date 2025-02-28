a44d852dc11f1d85c0a7f5ede89694c7434fd142
Seggan <seggan21@gmail.com>
2025-02-28 15:18:29 -0500

    Merge classes

---
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/item/SimpleItemSchema.kt
@@ -0,0 +1,22 @@
+package io.github.pylonmc.pylon.core.item
+
+import io.github.pylonmc.pylon.core.block.PylonBlockSchema
+import io.github.pylonmc.pylon.core.recipe.RecipeType
+import io.github.pylonmc.pylon.core.registry.PylonRegistry
+import io.github.pylonmc.pylon.core.registry.RegistryHandler
+import org.bukkit.Keyed
+import org.bukkit.NamespacedKey
+import org.bukkit.inventory.ItemStack
+
+class SimpleItemSchema<R : Keyed> @JvmOverloads constructor(
+    id: NamespacedKey,
+    template: ItemStack,
+    private val recipeType: RecipeType<R>,
+    private val recipe: (ItemStack) -> R,
+    private val block: PylonBlockSchema? = null
+) : PylonItemSchema(id, SimplePylonItem::class.java, template), RegistryHandler<SimpleItemSchema<R>> {
+    override fun onRegister(registry: PylonRegistry<SimpleItemSchema<R>>) {
+        recipeType.addRecipe(recipe(itemStack))
+        block?.register()
+    }
+}
