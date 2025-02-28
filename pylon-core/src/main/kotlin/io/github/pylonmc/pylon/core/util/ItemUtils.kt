1f94757b9360455c8f76185b6f22f9042d084f75
Seggan <seggan21@gmail.com>
2025-02-28 15:43:33 -0500

    Keyed utils

---
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/util/ItemUtils.kt
@@ -0,0 +1,27 @@
+@file:JvmName("ItemUtils")
+
+package io.github.pylonmc.pylon.core.util
+
+import io.github.pylonmc.pylon.core.item.PylonItem
+import io.github.pylonmc.pylon.core.registry.PylonRegistry
+import org.bukkit.Material
+import org.bukkit.NamespacedKey
+import org.bukkit.inventory.ItemStack
+
+val ItemStack.itemKey: NamespacedKey
+    get() {
+        val pylonItem = PylonItem.fromStack(this)
+        return pylonItem?.schema?.key ?: this.type.key
+    }
+
+private val keyToMaterial = Material.entries.associateBy { it.key }
+
+fun keyToItem(key: NamespacedKey): ItemStack {
+    val pylonItem = PylonRegistry.ITEMS[key]?.itemStack
+    return if (pylonItem != null) {
+        pylonItem
+    } else {
+        val material = keyToMaterial[key] ?: throw IllegalArgumentException("Unknown item: $key")
+        ItemStack(material)
+    }
+}
