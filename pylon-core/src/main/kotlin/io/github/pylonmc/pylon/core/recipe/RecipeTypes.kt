dd8d5ef35630646e3f7953e390a403e22dd85240
Seggan <seggan21@gmail.com>
2025-03-19 15:30:03 -0400

    Fix crafting

--- pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/recipe/RecipeTypes.kt
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/recipe/RecipeTypes.kt
@@ -1,9 +1,16 @@
 package io.github.pylonmc.pylon.core.recipe
 
+import io.github.pylonmc.pylon.core.item.PylonItem
+import io.github.pylonmc.pylon.core.item.PylonItemSchema
+import io.github.pylonmc.pylon.core.pluginInstance
 import io.github.pylonmc.pylon.core.util.pylonKey
 import org.bukkit.Bukkit
 import org.bukkit.Keyed
 import org.bukkit.NamespacedKey
+import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
+import org.bukkit.event.Listener
+import org.bukkit.event.inventory.PrepareItemCraftEvent
 import org.bukkit.inventory.*
 
 object RecipeTypes {
@@ -15,7 +22,7 @@ object RecipeTypes {
     val VANILLA_CAMPFIRE = vanillaRecipeWrapper<CampfireRecipe>("vanilla_campfire")
 
     @JvmField
-    val VANILLA_CRAFTING = vanillaRecipeWrapper<CraftingRecipe>("vanilla_crafting")
+    val VANILLA_CRAFTING: RecipeType<CraftingRecipe> = CraftingRecipeType
 
     @JvmField
     val VANILLA_FURNACE = vanillaRecipeWrapper<FurnaceRecipe>("vanilla_furnace")
@@ -28,6 +35,33 @@ object RecipeTypes {
 
     @JvmField
     val VANILLA_STONECUTTING = vanillaRecipeWrapper<StonecuttingRecipe>("vanilla_stonecutting")
+}
+
+private object CraftingRecipeType : RecipeType<CraftingRecipe>(
+    NamespacedKey.minecraft("crafting")
+), Listener {
+
+    init {
+        Bukkit.getPluginManager().registerEvents(this, pluginInstance)
+    }
+
+    override fun registerRecipe(recipe: CraftingRecipe) {
+        Bukkit.addRecipe(recipe)
+    }
+
+    override fun unregisterRecipe(recipe: NamespacedKey) {
+        Bukkit.removeRecipe(recipe)
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    private fun onPreCraft(e: PrepareItemCraftEvent) {
+        val recipe = e.recipe
+        val inventory = e.inventory
+        if (recipes.all { it.key != recipe } && inventory.any { PylonItem.fromStack(it) != null }) {
+            // Prevent the erroneous crafting of vanilla items with Pylon ingredients
+            inventory.result = null
+        }
+    }
 }
 
 private fun <T> vanillaRecipeWrapper(keyStr: String): RecipeType<T>
