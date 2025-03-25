41a2f94839364b292de683095c224a4028ab21a7
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-03-25 16:25:06 -0400

    use player instead of humanentity

--- pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/item/base/InventoryItem.kt
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/item/base/InventoryItem.kt
@@ -1,14 +1,14 @@
 package io.github.pylonmc.pylon.core.item.base
 
-import org.bukkit.entity.HumanEntity
+import org.bukkit.entity.Player
 
 interface InventoryItem {
     /**
      * Called when the item enters the player's inventory.
      */
-    fun onEnterInventory(player: HumanEntity)
+    fun onEnterInventory(player: Player)
     /**
      * Called when the item exits the player's inventory.
      */
-    fun onExitInventory(player: HumanEntity)
+    fun onExitInventory(player: Player)
 }
