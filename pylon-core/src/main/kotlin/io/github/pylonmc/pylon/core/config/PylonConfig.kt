a72ec70c72b288ff5d7c98c9881d3d93acc27ab2
LordIdra <percy.baker.0@gmail.com>
2025-03-25 00:42:46 +0000

    Add PylonConfig

---
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/config/PylonConfig.kt
@@ -0,0 +1,14 @@
+package io.github.pylonmc.pylon.core.config
+
+import io.github.pylonmc.pylon.core.pluginInstance
+
+object PylonConfig {
+
+    private val config = Config(pluginInstance, "config.yml")
+
+    @JvmField
+    val tickDelay = config.get("tick-delay", 10)
+
+    @JvmField
+    val allowedBlockErrors = config.get("tick-delay", 10)
+}
