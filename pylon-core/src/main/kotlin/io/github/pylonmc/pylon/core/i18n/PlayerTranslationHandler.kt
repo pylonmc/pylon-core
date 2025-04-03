86cee9b5c3c430c4103894d7bc313bffb3e1bd41
Seggan <seggan21@gmail.com>
2025-04-03 17:48:44 -0400

    Basic name translation

--- pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/i18n/PlayerTranslationHandler.kt
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/i18n/PlayerTranslationHandler.kt
@@ -2,11 +2,15 @@ package io.github.pylonmc.pylon.core.i18n
 
 import io.github.pylonmc.pylon.core.item.PylonItem
 import io.papermc.paper.datacomponent.DataComponentTypes
+import net.kyori.adventure.translation.GlobalTranslator
 import org.bukkit.entity.Player
 
+@Suppress("UnstableApiUsage")
 class PlayerTranslationHandler(val player: Player) {
 
     fun handleItem(item: PylonItem<*>) {
-        val name = item.stack.getData(DataComponentTypes.ITEM_NAME)
+        val name = item.stack.getData(DataComponentTypes.ITEM_NAME) ?: return
+        val translated = GlobalTranslator.render(name, player.locale())
+        item.stack.setData(DataComponentTypes.ITEM_NAME, translated)
     }
 }
