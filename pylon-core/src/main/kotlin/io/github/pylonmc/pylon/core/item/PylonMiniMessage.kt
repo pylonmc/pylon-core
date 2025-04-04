a90314e443eacf30c44d013f908ee9e5230ed9f8
Seggan <seggan21@gmail.com>
2025-04-04 17:09:20 -0400

    Copy stuff from data driven items branch

---
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/item/PylonMiniMessage.kt
@@ -0,0 +1,52 @@
+@file:JvmName("PylonMiniMessage")
+
+package io.github.pylonmc.pylon.core.item
+
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextColor
+import net.kyori.adventure.text.format.TextDecoration
+import net.kyori.adventure.text.minimessage.Context
+import net.kyori.adventure.text.minimessage.MiniMessage
+import net.kyori.adventure.text.minimessage.tag.Tag
+import net.kyori.adventure.text.minimessage.tag.resolver.ArgumentQueue
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver
+
+val customMiniMessage = MiniMessage.builder()
+    .tags(TagResolver.standard())
+    .editTags {
+        it.tag("arrow", ::arrow)
+        it.tag("lore", Tag.styling(TextDecoration.ITALIC.withState(false), NamedTextColor.GRAY))
+        it.tag(setOf("instruction", "insn")) { _, _ -> Tag.styling(TextColor.color(0xf9d104)) }
+        it.tag(setOf("attribute", "attr"), ::attr)
+        it.tag(setOf("quantity", "q"), ::quantity)
+    }
+    .strict(false)
+    .build()
+
+private fun arrow(args: ArgumentQueue, @Suppress("unused") ctx: Context): Tag {
+    val color = args.peek()?.value()?.let(::parseColor) ?: TextColor.color(0x666666)
+    return Tag.selfClosingInserting(Component.text("\u2192").color(color))
+}
+
+private fun attr(args: ArgumentQueue, @Suppress("unused") ctx: Context): Tag {
+    val name = args.popOr("Attribute name not present").value()
+    return Tag.inserting(
+        Component.text()
+            .color(NamedTextColor.WHITE)
+            .append(Component.text("$name:").color(TextColor.color(0xa9d9e8)))
+    )
+}
+
+private fun quantity(args: ArgumentQueue, @Suppress("unused") ctx: Context): Tag {
+    val name = args.popOr("Quantity name not present").value()
+    val quantity = Quantity.byName(name) ?: throw ctx.newException("Quantity '$name' not found", args)
+    return Tag.inserting(ctx.deserialize(quantity))
+}
+
+private fun parseColor(color: String): TextColor {
+    val theOnlyTrueWayToSpellGray = color.replace("grey", "gray")
+    return TextColor.fromHexString(theOnlyTrueWayToSpellGray)
+        ?: NamedTextColor.NAMES.value(theOnlyTrueWayToSpellGray)
+        ?: throw IllegalArgumentException("No such color: $color")
+}
