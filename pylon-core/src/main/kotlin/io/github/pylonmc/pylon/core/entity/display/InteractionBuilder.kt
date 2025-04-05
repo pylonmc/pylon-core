99c4a18fa904e7322320eac020e680dd15d949e0
LordIdra <percy.baker.0@gmail.com>
2025-04-05 01:03:50 +0100

    Interaction builder

---
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/entity/display/InteractionBuilder.kt
@@ -0,0 +1,32 @@
+package io.github.pylonmc.pylon.core.entity.display
+
+import org.bukkit.Location
+import org.bukkit.entity.Interaction
+
+class InteractionBuilder {
+    var width: Float?
+    var height: Float?
+
+    constructor(other: InteractionBuilder) {
+        this.width = other.width
+        this.height = other.height
+    }
+
+    fun build(location: Location): Interaction {
+        val finalLocation = location.clone()
+        // Account for Mojang deciding to center the entity on Y but not X and Z for some reason
+        if (height != null) {
+            finalLocation.subtract(0.0, height!! / 2.0, 0.0)
+        }
+        return location.getWorld().spawn(finalLocation, Interaction::class.java, this::update)
+    }
+
+    fun update(interaction: Interaction) {
+        if (width != null) {
+            interaction.interactionWidth = width!!
+        }
+        if (height != null) {
+            interaction.interactionHeight = height!!
+        }
+    }
+}
