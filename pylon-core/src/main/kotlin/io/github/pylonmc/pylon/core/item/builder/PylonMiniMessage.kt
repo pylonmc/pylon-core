526993f2fabab8cc1f0dce4856d6ed3ae1b2e0a1
Seggan <seggan21@gmail.com>
2025-04-08 19:35:12 -0400

    No break

--- pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/item/builder/PylonMiniMessage.kt
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/item/builder/PylonMiniMessage.kt
@@ -3,11 +3,13 @@
 package io.github.pylonmc.pylon.core.item.builder
 
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.TextReplacementConfig
 import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.format.TextColor
 import net.kyori.adventure.text.format.TextDecoration
 import net.kyori.adventure.text.minimessage.Context
 import net.kyori.adventure.text.minimessage.MiniMessage
+import net.kyori.adventure.text.minimessage.tag.Modifying
 import net.kyori.adventure.text.minimessage.tag.Tag
 import net.kyori.adventure.text.minimessage.tag.resolver.ArgumentQueue
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver
@@ -20,6 +22,7 @@ val customMiniMessage = MiniMessage.builder()
         it.tag(setOf("instruction", "insn")) { _, _ -> Tag.styling(TextColor.color(0xf9d104)) }
         it.tag(setOf("attribute", "attr"), ::attr)
         it.tag(setOf("quantity", "q"), ::quantity)
+        it.tag(setOf("nbsp", "nb"), ::nbsp)
     }
     .strict(false)
     .build()
@@ -42,6 +45,18 @@ private fun quantity(args: ArgumentQueue, @Suppress("unused") ctx: Context): Tag
     val name = args.popOr("Quantity name not present").value()
     val quantity = Quantity.byName(name) ?: throw ctx.newException("Quantity '$name' not found", args)
     return Tag.inserting(ctx.deserialize(quantity))
+}
+
+@Suppress("unused")
+private fun nbsp(args: ArgumentQueue, ctx: Context): Tag {
+    val replacementConfig = TextReplacementConfig.builder()
+        .match(" ")
+        .replacement(Typography.nbsp.toString())
+        .build()
+    return Modifying { current, depth ->
+        if (depth != 0) Component.empty()
+        else current.replaceText(replacementConfig)
+    }
 }
 
 private fun parseColor(color: String): TextColor {
