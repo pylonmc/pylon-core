a4ec88c865b8e2a5ca43bb89149e283c25bd9a41
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-04-13 13:56:22 -0400

    impl player param

--- pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/PylonCommand.kt
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/PylonCommand.kt
@@ -37,12 +37,8 @@ object PylonCommand : BaseCommand() {
     @Subcommand("debug")
     @Description("Gives you the pylon debugging item to view pylon data")
     @CommandPermission("pylon.command.debug")
-    fun debug() {
-        if(!currentCommandIssuer.isPlayer){
-            currentCommandIssuer.sendMessage("This should only be run on a client!")
-            return
-        }
-        currentCommandIssuer.getIssuer<Player>().give(DebugWaxedWeatheredCutCopperStairs.itemStack)
+    fun debug(player: Player) {
+        player.give(DebugWaxedWeatheredCutCopperStairs.itemStack)
     }
 
     @Private
