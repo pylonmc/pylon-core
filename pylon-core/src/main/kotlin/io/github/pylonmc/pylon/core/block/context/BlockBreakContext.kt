61bd81a6321cec2d607cca574fcfa603c53e7d5a
Seggan <seggan21@gmail.com>
2025-04-22 19:31:20 -0400

    `block.context`

--- pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/block/context/BlockBreakContext.kt
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/block/context/BlockBreakContext.kt
@@ -1,11 +1,15 @@
 package io.github.pylonmc.pylon.core.block.context
 
+import io.github.pylonmc.pylon.core.block.BlockStorage
 import org.bukkit.GameMode
 import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.block.BlockBurnEvent
 import org.bukkit.event.block.BlockExplodeEvent
 import org.bukkit.event.block.BlockFadeEvent
 
+/**
+ * The reason a Pylon block was broken
+ */
 interface BlockBreakContext {
 
     /**
@@ -13,6 +17,9 @@ interface BlockBreakContext {
      */
     val normallyDrops: Boolean
 
+    /**
+     * The block was removed by a plugin calling [BlockStorage.breakBlock]
+     */
     data object PluginBreak : BlockBreakContext {
         override val normallyDrops = true
     }
@@ -22,11 +29,19 @@ interface BlockBreakContext {
             get() = event.player.gameMode != GameMode.CREATIVE
     }
 
+    /**
+     * Unlike [Exploded], this context means that the block was exploded by an entity
+     * or another block exploding it
+     */
     // No event parameter since explosion can be either from block or entity
     data object WasExploded : BlockBreakContext {
         override val normallyDrops = true
     }
 
+    /**
+     * Unlike [WasExploded], this context means that the *block itself* exploded,
+     * i.e., the block was a TNT
+     */
     data class Exploded(val event: BlockExplodeEvent) : BlockBreakContext {
         override val normallyDrops = false
     }
@@ -35,6 +50,9 @@ interface BlockBreakContext {
         override val normallyDrops = false
     }
 
+    /**
+     * See [BlockFadeEvent] for information on what counts as "fading"
+     */
     data class Faded(val event: BlockFadeEvent) : BlockBreakContext {
         override val normallyDrops = false
     }
--- pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/block/context/BlockCreateContext.kt
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/block/context/BlockCreateContext.kt
@@ -3,9 +3,18 @@ package io.github.pylonmc.pylon.core.block.context
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 
+/**
+ * The reason a Pylon block was created
+ */
 interface BlockCreateContext {
 
+    /**
+     * A player placed the block
+     */
     data class PlayerPlace(val player: Player, val item: ItemStack) : BlockCreateContext
 
+    /**
+     * No other reason was given
+     */
     data object Default : BlockCreateContext
 }
