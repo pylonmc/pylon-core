011ddf39aa227caf9da73d9b69c318aace3a58b0
LordIdra <percy.baker.0@gmail.com>
2025-05-23 23:00:05 +0100

    Change block placement to be inherent to pylonItem

--- pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/item/PylonItemSchema.kt
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/item/PylonItemSchema.kt
@@ -1,21 +1,29 @@
 package io.github.pylonmc.pylon.core.item
 
+import io.github.pylonmc.pylon.core.block.BlockStorage
+import io.github.pylonmc.pylon.core.block.PylonBlock
+import io.github.pylonmc.pylon.core.block.PylonBlockSchema
+import io.github.pylonmc.pylon.core.block.context.BlockCreateContext
 import io.github.pylonmc.pylon.core.datatypes.PylonSerializers
 import io.github.pylonmc.pylon.core.registry.RegistryHandler
 import io.github.pylonmc.pylon.core.util.findConstructorMatching
 import io.github.pylonmc.pylon.core.util.pylonKey
 import org.bukkit.Keyed
 import org.bukkit.NamespacedKey
+import org.bukkit.block.Block
 import org.bukkit.inventory.ItemStack
 import java.lang.invoke.MethodHandle
 
-class PylonItemSchema internal constructor(
+class PylonItemSchema @JvmOverloads internal constructor(
     @JvmSynthetic internal val itemClass: Class<out PylonItem>,
-    private val template: ItemStack
+    private val template: ItemStack,
+    private val pylonBlock: PylonBlockSchema? = null
 ) : Keyed, RegistryHandler {
 
     private val key = template.persistentDataContainer.get(idKey, PylonSerializers.NAMESPACED_KEY)
         ?: throw IllegalArgumentException("Provided item stack is not a Pylon item; make sure you are using ItemStackBuilder.defaultBuilder to create the item stack")
+
+    val addon = PylonItem.getAddon(key)
 
     val itemStack: ItemStack
         get() = template.clone()
@@ -29,6 +37,17 @@ class PylonItemSchema internal constructor(
     ) ?: throw NoSuchMethodException(
         "Item '$key' ($itemClass) is missing a load constructor (${javaClass.simpleName}, ItemStack)"
     )
+
+    fun doPlace(context: BlockCreateContext, block: Block): PylonBlock<*>? {
+        if (pylonBlock == null) {
+            return null
+        }
+        check(template.type.isBlock) { "Material ${template.type} is not a block" }
+        if (BlockStorage.isPylonBlock(block)) { // special case: you can place on top of structure void blocks
+            return null
+        }
+        return BlockStorage.placeBlock(block, pylonBlock, context)
+    }
 
     override fun getKey(): NamespacedKey = key
 
