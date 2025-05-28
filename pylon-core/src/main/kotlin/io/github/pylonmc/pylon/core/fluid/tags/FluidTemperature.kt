31167f0eb12e18f67c76dcd3bc58fb47d877ed05
Seggan <seggan21@gmail.com>
2025-05-28 14:24:27 -0400

    New temperature system

--- pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/fluid/tags/FluidTemperature.kt
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/fluid/tags/FluidTemperature.kt
@@ -1,8 +1,12 @@
 package io.github.pylonmc.pylon.core.fluid.tags
 
 import io.github.pylonmc.pylon.core.fluid.PylonFluidTag
+import net.kyori.adventure.text.Component
 
-/**
- * Temperature in celsius, the superior unit of measurement
- */
-data class FluidTemperature(val temperature: Double) : PylonFluidTag
+enum class FluidTemperature : PylonFluidTag {
+    COLD,
+    NORMAL,
+    HOT;
+
+    val displayName: Component = Component.translatable("pylon.pyloncore.fluid.temperature.${this.name.lowercase()}")
+}
