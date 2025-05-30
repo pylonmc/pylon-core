275a070af5cb0f1ec827e1eea81eba1b1e1f113c
LordIdra <percy.baker.0@gmail.com>
2025-05-30 19:10:14 +0100

    Check blocks to see if WAILA key exists

--- pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/block/PhantomBlock.kt
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/block/PhantomBlock.kt
@@ -51,7 +51,7 @@ class PhantomBlock(
 
     override fun getWaila(player: Player): WailaConfig {
         return WailaConfig(
-            text = name,
+            key,
             placeholders = mapOf("block" to Component.text(erroredBlockKey.toString())),
             color = BossBar.Color.RED
         )
--- pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/block/waila/WailaConfig.kt
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/block/waila/WailaConfig.kt
@@ -3,10 +3,11 @@ package io.github.pylonmc.pylon.core.block.waila
 import io.github.pylonmc.pylon.core.i18n.PlaceholderAttacher
 import net.kyori.adventure.bossbar.BossBar
 import net.kyori.adventure.text.Component
+import org.bukkit.NamespacedKey
 import org.bukkit.entity.Player
 
 data class WailaConfig @JvmOverloads constructor(
-    val text: Component,
+    val key: NamespacedKey,
     val placeholders: Map<String, Component> = emptyMap(),
     val color: BossBar.Color = BossBar.Color.WHITE,
     val style: BossBar.Overlay = BossBar.Overlay.PROGRESS,
@@ -17,12 +18,17 @@ data class WailaConfig @JvmOverloads constructor(
         val player = bar.viewers().singleOrNull() as? Player
         if (player != null) {
             val attacher = PlaceholderAttacher(placeholders)
-            bar.name(attacher.render(text, Unit))
+            bar.name(attacher.render(Component.translatable(getWailaTranslationKey(key)), Unit))
         } else {
-            bar.name(text)
+            bar.name(Component.translatable(getWailaTranslationKey(key)))
         }
         bar.color(color)
         bar.overlay(style)
         bar.progress(progress)
+    }
+
+    companion object {
+        fun getWailaTranslationKey(key: NamespacedKey): String
+            = "pylon.${key.namespace}.block.${key.key}"
     }
 }
