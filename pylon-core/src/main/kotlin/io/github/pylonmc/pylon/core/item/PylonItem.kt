4dbd5158d4372bf7082b34dc2d91557a0c72adf0
LordIdra <percy.baker.0@gmail.com>
2025-05-30 18:46:35 +0100

    Add placeholder warnings

--- pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/item/PylonItem.kt
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/item/PylonItem.kt
@@ -1,9 +1,13 @@
 package io.github.pylonmc.pylon.core.item
 
+import io.github.pylonmc.pylon.core.PylonCore
 import io.github.pylonmc.pylon.core.config.Settings
 import io.github.pylonmc.pylon.core.datatypes.PylonSerializers
+import io.github.pylonmc.pylon.core.i18n.AddonTranslator
+import io.github.pylonmc.pylon.core.item.builder.ItemStackBuilder
 import io.github.pylonmc.pylon.core.registry.PylonRegistry
 import net.kyori.adventure.text.ComponentLike
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
 import org.bukkit.Keyed
 import org.bukkit.NamespacedKey
 import org.bukkit.inventory.ItemStack
@@ -34,15 +38,48 @@ open class PylonItem(val stack: ItemStack) : Keyed {
 
     companion object {
 
-        @JvmStatic
-        fun register(itemClass: Class<out PylonItem>, template: ItemStack) {
-            PylonRegistry.ITEMS.register(PylonItemSchema(itemClass, template))
+        private fun register(schema: PylonItemSchema) {
+            val translator = AddonTranslator.translators[schema.addon]
+            check(translator != null) {
+                "Addon does not have a translator; did you forget to call registerWithPylon()?"
+            }
+
+            // Adventure is a perfect API with absolutely no problems whatsoever.
+            val plaintextName = PlainTextComponentSerializer.plainText().serialize(schema.itemStack.effectiveName())
+            val plaintextLore = schema.itemStack.lore()?.get(0)?.let { PlainTextComponentSerializer.plainText().serialize(it) }
+
+            var isNameAndLoreValid = true
+            if (plaintextName != ItemStackBuilder.nameKey(schema.key)) {
+                PylonCore.logger.warning("Item ${schema.key}'s name is not a translation key; check your item uses ItemStackBuilder.pylonItem(...)")
+                isNameAndLoreValid = false
+            }
+
+            if (plaintextLore == null || plaintextLore != ItemStackBuilder.loreKey(schema.key)) {
+                PylonCore.logger.warning("Item ${schema.key}'s lore is not a translation key; check your item uses ItemStackBuilder.pylonItem(...)")
+                isNameAndLoreValid = false
+            }
+
+            if (isNameAndLoreValid) {
+                for (locale in schema.addon.languages) {
+                    if (!translator.translationKeyExists(plaintextName, locale)) {
+                        PylonCore.logger.warning("Item ${schema.key} is missing a name translation key (${locale.displayName} | ${ItemStackBuilder.nameKey(schema.key)}")
+                    }
+                    if (!translator.translationKeyExists(plaintextLore!!, locale)) {
+                        PylonCore.logger.warning("Item ${schema.key} is missing a lore translation key (${locale.displayName} | ${ItemStackBuilder.loreKey(schema.key)}")
+                    }
+                }
+            }
+
+            PylonRegistry.ITEMS.register(schema)
         }
 
         @JvmStatic
-        fun register(itemClass: Class<out PylonItem>, template: ItemStack, pylonBlockKey: NamespacedKey) {
-            PylonRegistry.ITEMS.register(PylonItemSchema(itemClass, template, pylonBlockKey))
-        }
+        fun register(itemClass: Class<out PylonItem>, template: ItemStack)
+            = register(PylonItemSchema(itemClass, template))
+
+        @JvmStatic
+        fun register(itemClass: Class<out PylonItem>, template: ItemStack, pylonBlockKey: NamespacedKey)
+            = register(PylonItemSchema(itemClass, template, pylonBlockKey))
 
         /**
          * Converts a regular ItemStack to a PylonItemStack
