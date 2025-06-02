7daa7f83acea6db4d88fe0db6c9bb8b002ec5ee5
LordIdra <percy.baker.0@gmail.com>
2025-06-02 17:54:29 +0100

    cast to translatablecomponent instead of serializing

--- pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/item/PylonItem.kt
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/item/PylonItem.kt
@@ -6,8 +6,10 @@ import io.github.pylonmc.pylon.core.datatypes.PylonSerializers
 import io.github.pylonmc.pylon.core.i18n.AddonTranslator
 import io.github.pylonmc.pylon.core.item.builder.ItemStackBuilder
 import io.github.pylonmc.pylon.core.registry.PylonRegistry
+import io.papermc.paper.datacomponent.DataComponentTypes
 import net.kyori.adventure.text.ComponentLike
-import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
+import net.kyori.adventure.text.TranslatableComponent
+import org.bukkit.Bukkit
 import org.bukkit.Keyed
 import org.bukkit.NamespacedKey
 import org.bukkit.inventory.ItemStack
@@ -45,26 +47,26 @@ open class PylonItem(val stack: ItemStack) : Keyed {
             }
 
             // Adventure is a perfect API with absolutely no problems whatsoever.
-            val name = PlainTextComponentSerializer.plainText().serialize(schema.itemStack.effectiveName())
-            val lore = schema.itemStack.lore()?.get(0)?.let { PlainTextComponentSerializer.plainText().serialize(it) }
+            val name = schema.itemStack.getData(DataComponentTypes.ITEM_NAME) as? TranslatableComponent
+            val lore = schema.itemStack.getData(DataComponentTypes.LORE)?.lines()?.get(0) as? TranslatableComponent
 
             var isNameAndLoreValid = true
-            if (name != ItemStackBuilder.nameKey(schema.key)) {
+            if (name == null || name.key() != ItemStackBuilder.nameKey(schema.key)) {
                 PylonCore.logger.warning("Item ${schema.key}'s name is not a translation key; check your item uses ItemStackBuilder.pylonItem(...)")
                 isNameAndLoreValid = false
             }
 
-            if (lore == null || lore != ItemStackBuilder.loreKey(schema.key)) {
+            if (lore == null || lore.key() != ItemStackBuilder.loreKey(schema.key)) {
                 PylonCore.logger.warning("Item ${schema.key}'s lore is not a translation key; check your item uses ItemStackBuilder.pylonItem(...)")
                 isNameAndLoreValid = false
             }
 
             if (isNameAndLoreValid) {
                 for (locale in schema.addon.languages) {
-                    if (!translator.translationKeyExists(name, locale)) {
+                    if (!translator.translationKeyExists(name!!.key(), locale)) {
                         PylonCore.logger.warning("Item ${schema.key} is missing a name translation key (${locale.displayName} | ${ItemStackBuilder.nameKey(schema.key)}")
                     }
-                    if (!translator.translationKeyExists(lore!!, locale)) {
+                    if (!translator.translationKeyExists(lore!!.key(), locale)) {
                         PylonCore.logger.warning("Item ${schema.key} is missing a lore translation key (${locale.displayName} | ${ItemStackBuilder.loreKey(schema.key)}")
                     }
                 }
