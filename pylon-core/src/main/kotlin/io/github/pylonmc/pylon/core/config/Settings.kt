fa7d18f807d3e09ff6f206dd7364c5002165945d
Seggan <seggan21@gmail.com>
2025-07-09 12:39:22 -0400

    `config`

--- pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/config/Settings.kt
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/config/Settings.kt
@@ -5,7 +5,10 @@ import org.bukkit.NamespacedKey
 
 object Settings {
 
+    /**
+     * Retrieves the settings for the given [key]
+     */
     @JvmStatic
-    fun get(key: NamespacedKey): Config
-            = getAddon(key).mergeGlobalConfig("settings/${key.key}.yml", "settings/${key.namespace}/${key.key}.yml")
+    fun get(key: NamespacedKey): Config =
+        getAddon(key).mergeGlobalConfig("settings/${key.key}.yml", "settings/${key.namespace}/${key.key}.yml")
 }
