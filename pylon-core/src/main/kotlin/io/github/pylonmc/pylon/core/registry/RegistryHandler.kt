340b4867e687e314f8ed14d55970b386b5427b1b
Seggan <seggan21@gmail.com>
2025-07-12 17:06:03 -0400

    `registry`

--- pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/registry/RegistryHandler.kt
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/registry/RegistryHandler.kt
@@ -1,5 +1,8 @@
 package io.github.pylonmc.pylon.core.registry
 
+/**
+ * Implement this interface to detect when something is registered or unregistered in a [PylonRegistry]
+ */
 interface RegistryHandler {
 
     fun onRegister(registry: PylonRegistry<*>) {}
