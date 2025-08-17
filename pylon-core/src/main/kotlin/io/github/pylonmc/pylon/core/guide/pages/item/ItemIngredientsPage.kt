6644f2bdac4f6fd49ee61f7670fa139fdad988cb
balugaq <129668183+balugaq@users.noreply.github.com>
2025-08-17 10:11:50 +0800

    Update ItemIngredientsPage.kt

--- pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/guide/pages/item/ItemIngredientsPage.kt
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/guide/pages/item/ItemIngredientsPage.kt
@@ -1,10 +1,13 @@
 package io.github.pylonmc.pylon.core.guide.pages.item
 
+import io.github.pylonmc.pylon.core.content.guide.PylonGuide
 import io.github.pylonmc.pylon.core.guide.button.BackButton
+import io.github.pylonmc.pylon.core.guide.button.PageButton
 import io.github.pylonmc.pylon.core.guide.pages.base.GuidePage
 import io.github.pylonmc.pylon.core.i18n.PylonArgument
 import io.github.pylonmc.pylon.core.item.builder.ItemStackBuilder
 import io.github.pylonmc.pylon.core.recipe.FluidOrItem
+import io.github.pylonmc.pylon.core.recipe.IngredientCalculation
 import io.github.pylonmc.pylon.core.recipe.IngredientCalculator
 import io.github.pylonmc.pylon.core.util.gui.GuiItems
 import io.github.pylonmc.pylon.core.util.pylonKey
@@ -17,38 +20,31 @@ import xyz.xenondevs.invui.gui.Gui
 import xyz.xenondevs.invui.gui.PagedGui
 import xyz.xenondevs.invui.gui.structure.Markers
 import xyz.xenondevs.invui.item.Item
-import xyz.xenondevs.invui.item.ItemProvider
 import xyz.xenondevs.invui.item.impl.SimpleItem
 import kotlin.math.max
 
 @Suppress("UnstableApiUsage")
 open class ItemIngredientsPage(val stack: ItemStack) : GuidePage {
-
-    val pages: MutableList<Gui> = mutableListOf()
-
-    init {
-        val calculation = IngredientCalculator.calculateFinal(stack)
-        val maxPage = max(calculation.inputs.size / 27, calculation.alongProducts.size / 9)
-        for (i in 0..maxPage) {
-            pages += getPage(stack, calculation, i)
-        }
-    }
-
     override val item = ItemStackBuilder.of(stack)
 
     override fun getKey() = KEY
 
     // page is 0 based
-    open fun getPage(stack: ItemStack, calculation: IngredientCalculator.IngredientCalculation, page: Int) =
+    open fun getPage(player: Player, stack: ItemStack, calculation: IngredientCalculation, page: Int, maxPage: Int) =
         PagedGui.guis()
             .setStructure(
+                "i i i i i i i i i",
                 "i i i i i i i i i",
                 "i i i i i i i i i",
                 "i i i i i i i i i",
                 "b f x x x x x a x",
                 "o o o o o o o o o",
             )
-            .addIngredient('x', GuiItems.background())
+            .addIngredient('#', GuiItems.background())
+            .addIngredient('<', if (maxPage > 1) GuiItems.pagePrevious() else GuiItems.background())
+            .addIngredient('b', BackButton(player))
+            .addIngredient('s', PageButton(PylonGuide.searchItemsAndFluidsPage))
+            .addIngredient('>', if (maxPage > 1) GuiItems.pageNext() else GuiItems.background())
             .addIngredient('f', info(stack, calculation.outputAmount))
             .addIngredient(
                 'a', if (!calculation.alongProducts.isEmpty()) {
@@ -58,13 +54,13 @@ open class ItemIngredientsPage(val stack: ItemStack) : GuidePage {
                 }
             )
             .addModifier {
-                for (i in 0..26) {
-                    it.setItem(i, flatAndAmount(calculation.inputs.getOrNull(27 * page + i)))
+                for (i in 0..35) {
+                    it.setItem(i, flatWithAmount(calculation.inputs.getOrNull(36 * page + i)))
                 }
             }
             .addModifier {
-                for (i in 0..7) {
-                    it.setItem(36 + i, flatAndAmount(calculation.alongProducts.getOrNull(9 * page + i)))
+                for (i in 0..8) {
+                    it.setItem(45 + i, flatWithAmount(calculation.alongProducts.getOrNull(9 * page + i)))
                 }
             }
             .build()
@@ -85,6 +81,13 @@ open class ItemIngredientsPage(val stack: ItemStack) : GuidePage {
         .addIngredient('x', Markers.CONTENT_LIST_SLOT_HORIZONTAL)
 
     override fun getGui(player: Player): Gui {
+        val pages = mutableListOf<Gui>()
+        val calculation = IngredientCalculator.calculateFinal(stack)
+        val maxPage = max(calculation.inputs.size / 36, calculation.alongProducts.size / 9)
+        for (i in 0..maxPage) {
+            pages += getPage(player, stack, calculation, i, maxPage)
+        }
+
         val gui = getHeader(player, pages)
         for (page in pages) {
             gui.addContent(page)
@@ -106,7 +109,7 @@ open class ItemIngredientsPage(val stack: ItemStack) : GuidePage {
         )
         .build()
 
-    fun flatAndAmount(fluidOrItem: FluidOrItem?): Item {
+    fun flatWithAmount(fluidOrItem: FluidOrItem?): Item {
         fluidOrItem ?: return GuiItems.background()
         return SimpleItem(
             when (fluidOrItem) {
