2e1953683e86c8e3e6ab86809a45549add5cf3d6
Seggan <seggan21@gmail.com>
2025-09-16 14:40:42 -0400

    MOAR TAGS

--- pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/item/PylonItemTag.kt
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/item/PylonItemTag.kt
@@ -1,11 +1,15 @@
 package io.github.pylonmc.pylon.core.item
 
+import io.github.pylonmc.pylon.core.registry.PylonRegistry
+import io.github.pylonmc.pylon.core.util.pylonKey
 import org.bukkit.Material
 import org.bukkit.NamespacedKey
 import org.bukkit.Tag
 import org.bukkit.inventory.ItemStack
 
 class PylonItemTag(private val key: NamespacedKey, items: Set<ItemTypeWrapper>) : Tag<ItemTypeWrapper> {
+
+    constructor(key: NamespacedKey, vararg materials: Material) : this(key, materials.map { ItemTypeWrapper(it) }.toSet())
 
     private val items = items.toMutableSet()
 
@@ -24,4 +28,24 @@ class PylonItemTag(private val key: NamespacedKey, items: Set<ItemTypeWrapper>) 
     override fun isTagged(item: ItemTypeWrapper): Boolean = item in items
     override fun getValues(): Set<ItemTypeWrapper> = items.toSet()
     override fun getKey(): NamespacedKey = key
+
+    @Suppress("unused")
+    companion object {
+        // @formatter:off
+        /**
+         * `pyloncore:rocks`:
+         * - [`#minecraft:base_stone_overworld`](https://minecraft.wiki/w/Block_tag_(Java_Edition)#base_stone_overworld)
+         * - `minecraft:cobblestone`
+         * - `minecraft:cobbled_deepslate`
+         * - `minecraft:blackstone`
+         */
+        @JvmField val ROCKS = PylonItemTag(
+            pylonKey("rocks"),
+            *Tag.BASE_STONE_OVERWORLD.values.toTypedArray(),
+            Material.COBBLESTONE,
+            Material.COBBLED_DEEPSLATE,
+            Material.BLACKSTONE
+        ).also { PylonRegistry.ITEM_TAGS.register(it) }
+        // @formatter:on
+    }
 }
