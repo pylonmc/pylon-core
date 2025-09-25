ff9e1f67f73227f06b851f3b86a344cb58e8e396
JustAHuman-xD <imjustahuman.xd@gmail.com>
2025-09-25 15:35:07 -0500

    updated culling preset adapter

--- pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/config/adapter/CullingPresetConfigAdapter.kt
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/config/adapter/CullingPresetConfigAdapter.kt
@@ -1,26 +1,22 @@
 package io.github.pylonmc.pylon.core.config.adapter
 
 import io.github.pylonmc.pylon.core.resourcepack.block.CullingPreset
-import org.bukkit.configuration.ConfigurationSection
 import java.lang.reflect.Type
 
 object CullingPresetConfigAdapter : ConfigAdapter<CullingPreset> {
     override val type: Type = CullingPreset::class.java
 
     override fun convert(value: Any): CullingPreset {
-        if (value is ConfigurationSection) {
-            return CullingPreset(
-                index = value.getInt("index"),
-                id = value.getString("id") ?: throw IllegalArgumentException("CullingPreset is missing 'id'"),
-                material = ConfigAdapter.MATERIAL.convert(value.getString("material")
-                    ?: throw IllegalArgumentException("CullingPreset is missing 'material'")),
-                hiddenInterval = value.getInt("hidden-interval", 1),
-                visibleInterval = value.getInt("visible-interval", 20),
-                alwaysShowRadius = value.getInt("always-show-radius", 16),
-                cullRadius = value.getInt("cull-radius", 64),
-                maxOccludingCount = value.getInt("max-occluding-count", 3)
-            )
-        }
-        throw IllegalArgumentException("Cannot convert $value to CullingPreset")
+        val map = MapConfigAdapter.STRING_TO_ANY.convert(value)
+        return CullingPreset(
+            index = ConfigAdapter.INT.convert(map["index"]!!),
+            id = ConfigAdapter.STRING.convert(map["id"]!!),
+            material = ConfigAdapter.MATERIAL.convert(map["material"]!!),
+            hiddenInterval = map["hidden-interval"]?.let { ConfigAdapter.INT.convert(it) } ?: 1,
+            visibleInterval = map["visible-interval"]?.let { ConfigAdapter.INT.convert(it) } ?: 20,
+            alwaysShowRadius = map["always-show-radius"]?.let { ConfigAdapter.INT.convert(it) } ?: 16,
+            cullRadius = map["cull-radius"]?.let { ConfigAdapter.INT.convert(it) } ?: 64,
+            maxOccludingCount = map["max-occluding-count"]?.let { ConfigAdapter.INT.convert(it) } ?: 3
+        )
     }
 }
