d02ded896c957a312133ccf49f3bb51d18bce1c7
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-09-25 21:33:05 -0400

    make javadoc

--- pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/entity/base/PylonCombustibleEntity.kt
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/entity/base/PylonCombustibleEntity.kt
@@ -3,6 +3,6 @@ package io.github.pylonmc.pylon.core.entity.base
 import org.bukkit.event.entity.EntityCombustEvent
 
 interface PylonCombustibleEntity {
-    // Could either be a EntityCombustByBlock or a EntityCombustByEntity event
+    /* Could either be a EntityCombustByBlock or a EntityCombustByEntity event */
     fun onCombust(event: EntityCombustEvent)
 }
