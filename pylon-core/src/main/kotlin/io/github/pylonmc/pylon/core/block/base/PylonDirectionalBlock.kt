102f9b6d3d050da4d785cb31ed6e2faaba2f17a2
JustAHuman-xD <imjustahuman.xd@gmail.com>
2025-09-28 14:04:58 -0500

    clarify doc

--- pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/block/base/PylonDirectionalBlock.kt
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/block/base/PylonDirectionalBlock.kt
@@ -1,9 +1,12 @@
 package io.github.pylonmc.pylon.core.block.base
 
+import io.github.pylonmc.pylon.core.block.PylonBlock
 import org.bukkit.block.BlockFace
 
 /**
  * Represents a block that has a specific facing direction.
+ *
+ * Internally only used for rotating [PylonBlock.blockTextureEntity]s.
  */
 interface PylonDirectionalBlock {
     fun getFacing(): BlockFace
