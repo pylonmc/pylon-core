0805f12eabca3ff4116d8b35f833496f688c34b8
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-10-13 16:05:36 -0400

    pr comments

--- pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/item/base/InventoryEffectItem.kt
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/item/base/InventoryEffectItem.kt
@@ -1,42 +1,49 @@
-package io.github.pylonmc.pylon.base.content.tools.base
+package io.github.pylonmc.pylon.core.item.base
 
 import io.github.pylonmc.pylon.core.PylonCore
 import io.github.pylonmc.pylon.core.item.PylonItem
-import io.github.pylonmc.pylon.core.item.base.PylonInventoryItem
 import org.bukkit.Bukkit
 import org.bukkit.NamespacedKey
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 import org.bukkit.persistence.PersistentDataType
 import org.bukkit.scheduler.BukkitTask
+import org.jetbrains.annotations.MustBeInvokedByOverriders
 import java.util.*
 
-abstract class InventoryEffectItem(stack: ItemStack) : PylonItem(stack), PylonInventoryItem {
+interface InventoryEffectItem : PylonInventoryItem {
     override fun onTick(player: Player, stack: ItemStack) {
-        tasks.putIfAbsent(effectKey, WeakHashMap())
-        tasks[effectKey]!![player.uniqueId]?.cancel()
-        if(!player.persistentDataContainer.has(effectKey)){
-            player.persistentDataContainer.set(effectKey, PersistentDataType.BOOLEAN, true)
-            applyEffect(player, stack)
+        tasks.putIfAbsent(itemKey, HashMap())
+        tasks[itemKey]!![player.uniqueId]?.cancel()
+        if (!player.persistentDataContainer.has(itemKey)) {
+            onAddedToInventory(player)
         }
-        tasks[effectKey]!![player.uniqueId] = Bukkit.getScheduler().runTaskLater(PylonCore.javaPlugin, Runnable {
-            player.persistentDataContainer.remove(effectKey)
-            removeEffect(player, stack) }, tickSpeed.tickRate)
+        tasks[itemKey]!![player.uniqueId] = Bukkit.getScheduler().runTaskLater(PylonCore.javaPlugin, Runnable {
+            onRemovedFromInventory(player)
+        }, tickSpeed.tickRate)
     }
 
     /**
-     * Remove the effect from the player.
+     * Remove the effect from the player. Best-effort removal therefore is no guarantee that the player is still connected or that the
+     * [PylonItem.stack] is up to date with the actual ItemStack when it runs.
      */
-    protected abstract fun removeEffect(player: Player, stack: ItemStack)
+    @MustBeInvokedByOverriders
+    fun onRemovedFromInventory(player: Player) {
+        player.persistentDataContainer.remove(itemKey)
+    }
 
     /**
      * Apply the effect of this item onto the player
      */
-    protected abstract fun applyEffect(player: Player, stack: ItemStack)
+    @MustBeInvokedByOverriders
+    fun onAddedToInventory(player: Player) {
+        player.persistentDataContainer.set(itemKey, PersistentDataType.BOOLEAN, true)
+    }
 
-    private val effectKey = NamespacedKey(key.namespace, key.key + "_effect")
+    val itemKey: NamespacedKey
+        get() = NamespacedKey((this as PylonItem).key.namespace, key.key + "_effect")
 
     companion object {
-        private val tasks = HashMap<NamespacedKey, WeakHashMap<UUID, BukkitTask>>()
+        private val tasks = HashMap<NamespacedKey, HashMap<UUID, BukkitTask>>()
     }
 }
