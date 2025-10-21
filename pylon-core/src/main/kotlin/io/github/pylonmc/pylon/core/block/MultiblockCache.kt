0b71a2da900184054772eaf1c25eae7cdf4590b0
Intybyte <vaan.testwork@gmail.com>
2025-10-21 12:18:01 +0200

    Fix pistons and add StructureGrowEvent support

--- pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/block/MultiblockCache.kt
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/block/MultiblockCache.kt
@@ -1,6 +1,9 @@
 package io.github.pylonmc.pylon.core.block
 
 import com.destroystokyo.paper.event.block.BlockDestroyEvent
+import com.github.shynixn.mccoroutine.bukkit.launch
+import com.github.shynixn.mccoroutine.bukkit.ticks
+import io.github.pylonmc.pylon.core.PylonCore
 import io.github.pylonmc.pylon.core.block.base.PylonMultiblock
 import io.github.pylonmc.pylon.core.event.PylonBlockBreakEvent
 import io.github.pylonmc.pylon.core.event.PylonBlockPlaceEvent
@@ -12,12 +15,15 @@ import io.github.pylonmc.pylon.core.event.PylonMultiblockUnformEvent
 import io.github.pylonmc.pylon.core.util.position.BlockPosition
 import io.github.pylonmc.pylon.core.util.position.ChunkPosition
 import io.github.pylonmc.pylon.core.util.position.position
+import kotlinx.coroutines.delay
 import org.bukkit.block.Block
+import org.bukkit.block.BlockState
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.block.*
 import org.bukkit.event.entity.EntityExplodeEvent
+import org.bukkit.event.world.StructureGrowEvent
 
 /**
  * This class does a lot and is quite dense and complicated. Here's the summary of what it does:
@@ -260,7 +266,9 @@ internal object MultiblockCache : Listener {
     // Otherwise, we would be able to handle blocks being moved by pistons and not have to cancel
     @EventHandler(ignoreCancelled = true)
     private fun blockUpdate(event: BlockPistonExtendEvent) {
-        if (event.blocks.any { loadedMultiblocksWithComponent(event.block).isNotEmpty() }) {
+        val blocksBefore = event.blocks
+        val blocksAfter = event.blocks.map { it.getRelative(event.direction) }
+        if (blocksBefore.any { loadedMultiblocksWithComponent(it).isNotEmpty() } || blocksAfter.any { loadedMultiblocksWithComponent(it).isNotEmpty()}) {
             event.isCancelled = true
         }
     }
@@ -270,8 +278,15 @@ internal object MultiblockCache : Listener {
     // Otherwise, we would be able to handle blocks being moved by pistons and not have to cancel
     @EventHandler(ignoreCancelled = true)
     private fun blockUpdate(event: BlockPistonRetractEvent) {
-        if (event.blocks.any { loadedMultiblocksWithComponent(event.block).isNotEmpty() }) {
+        val blocksBefore = event.blocks
+        val blocksAfter = event.blocks.map { it.getRelative(event.direction) }
+        if (blocksBefore.any { loadedMultiblocksWithComponent(it).isNotEmpty() } || blocksAfter.any { loadedMultiblocksWithComponent(it).isNotEmpty()}) {
             event.isCancelled = true
         }
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    private fun blockUpdate(event: StructureGrowEvent) {
+        event.blocks.map(BlockState::getBlock).forEach(this::onBlockModified)
     }
 }
