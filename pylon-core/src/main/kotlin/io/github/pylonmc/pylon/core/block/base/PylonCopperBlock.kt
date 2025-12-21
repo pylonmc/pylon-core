335e91dabb1f26f18cf2b8b051e502939e2d673a
Intybyte <vaan.testwork@gmail.com>
2025-12-21 11:08:07 +0100

    whoopsie

--- pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/block/base/PylonCopperBlock.kt
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/block/base/PylonCopperBlock.kt
@@ -27,14 +27,15 @@ interface PylonCopperBlock {
 
             val hand = event.hand ?: return
             val block = event.clickedBlock ?: return
+            val pylonBlock = BlockStorage.get(block) ?: return
 
             val stack = event.player.inventory.getItem(hand)
             val type = stack.type
 
+
             if (!Tag.ITEMS_AXES.isTagged(type) && type != Material.HONEYCOMB) return
             if (!block.type.toString().lowercase().contains("copper")) return
 
-            val pylonBlock = BlockStorage.get(block)
             if (pylonBlock is PylonCopperBlock) {
                 try {
                     pylonBlock.changeWaxing(event)
