38d592ce68b6e49c30f3796c7f2a0a0768dd980d
Intybyte <vaan.testwork@gmail.com>
2026-01-02 11:48:42 +0100

    Add docs to PylonEntityAddEvent

--- pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/event/PylonEntityAddEvent.kt
+++ pylon-core/src/main/kotlin/io/github/pylonmc/pylon/core/event/PylonEntityAddEvent.kt
@@ -4,6 +4,11 @@ import io.github.pylonmc.pylon.core.entity.PylonEntity
 import org.bukkit.event.Event
 import org.bukkit.event.HandlerList
 
+/**
+ * Called when an entity is added to [io.github.pylonmc.pylon.core.entity.EntityStorage],
+ * when this is called, the entity already spawned in the world, this only tracks when the
+ * pylon entity is actually added in the [io.github.pylonmc.pylon.core.entity.EntityStorage]
+ */
 class PylonEntityAddEvent(val pylonEntity: PylonEntity<*>) : Event() {
 
     override fun getHandlers(): HandlerList
